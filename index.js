d7cb69897991959a9264193557f1df5db9156b7a
csfx-py <csfxshreyas@gmail.com>
2022-01-07 18:00:43 +0530

    added translate text and fixed phone number typeerrors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const axios = require("axios");
+const translate = require("translate-google");
+const langCode = require("./util/langCode");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -22,7 +24,9 @@ bot.help((ctx) =>
   ctx.reply(`
 /start - Start the bot\n
 /help - Show this help\n
-/phone <10 digit number> - Get the phone number details`)
+/phone <10 digit number> - Get the phone number details\n
+/translate $<optional from> $<to> <text> - Translate the text. Default from is auto detect and to is en.
+example: /translate $english $hindi hello, how are you?\n`)
 );
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
@@ -44,29 +48,63 @@ bot.command("phone", async (ctx) => {
       },
     });
     usage++;
-    if(usage == 5){
+    if (usage == 5) {
       usage = 0;
       tc_index = (tc_index + 1) % TC_TOKENS_N;
     }
 
     return ctx.reply(
       `Name: ${res.data.name}\n
-    Business account? ${res.data.isBusiness}\n
-    Private account? ${res.data.isPrivate}\n
-    Image: ${res.data.image}\n
-    Phones: ${res.data.phones.map((phone) => phone.number).join(", ")}\n
-    Provider: ${res.data.phones.map((phone) => phone.info).join(", ")}\n
-    Address: ${res.data.addresses
-      .map((address) => address.address)
-      .join("\n")}\n
-    Email: ${res.data.email.email}\n
-    Facebook: ${res.data.facebook.uri}\n
-    Twitter: ${res.data.twitter.uri}\n`
+      Business account? ${res.data.isBusiness ? "Yes" : "No"}\n
+      Private account? ${res.data.isPrivate ? "Yes" : "No"}\n
+      Image: ${res.data.image}\n
+      Phones: ${res.data.phones.map((phone) => phone.number).join(", ")}\n
+      Provider: ${res.data.phones.map((phone) => phone.info).join(", ")}\n
+      Address: ${res.data.addresses
+        ?.map((address) => address.address)
+        .join("\n")}\n
+      Email: ${res.data.email?.email ? res.data.email.email : "N/A"}\n
+      Facebook: ${res.data.facebook?.uri ? res.data.facebook.uri : "N/A"}\n
+      Twitter: ${res.data.twitter?.uri ? res.data.twitter.uri : "N/A"}\n`
     );
   } catch (error) {
     console.log(error);
-    if (error.response.status === 429)
+    if (error.response?.status === 429)
       return ctx.reply("TrueCaller API rate limit exceeded");
+    return ctx.reply("Something went wrong");
+  }
+});
+
+bot.command("translate", async (ctx) => {
+  let from, to, text;
+  if (
+    ctx.message.text.split(" ")[1].startsWith("$") &&
+    ctx.message.text.split(" ")[2].startsWith("$")
+  ) {
+    from = ctx.message.text.split(" ")[1].substring(1);
+    to = ctx.message.text.split(" ")[2].substring(1);
+    text = ctx.message.text.split(" ").slice(3).join(" ");
+  } else if (
+    ctx.message.text.split(" ")[1].startsWith("$") &&
+    !ctx.message.text.split(" ")[2].startsWith("$")
+  ) {
+    from = "Automatic";
+    to = ctx.message.text.split(" ")[1].substring(1);
+    text = ctx.message.text.split(" ").slice(2).join(" ");
+  } else {
+    from = "Automatic";
+    to = "English";
+    text = ctx.message.text.split(" ").slice(1).join(" ");
+  }
+
+  from = langCode(from.charAt(0).toUpperCase() + from.slice(1));
+  to = langCode(to.charAt(0).toUpperCase() + to.slice(1));
+
+  try {
+    const res = await translate(text, { from, to });
+    return ctx.reply(res);
+  } catch (error) {
+    console.log(error);
     return ctx.reply("Something went wrong");
   }
 });
